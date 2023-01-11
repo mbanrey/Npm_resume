04970de792098a7d27a906f9b4fd22bee54987e8
Mitchell <mitchellbanrey@gmail.com>
2023-01-11 02:41:52 -0500

    v4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-fs.readFile('/npm_resume' + '/info.json', 'utf8', function(err, data) {
+fs.readFile('./npm_resume' + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
